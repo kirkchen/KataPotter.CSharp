990ed090f91ac08d096b4f8d918732214d5bf81e
Kirk Chen <rwk0119@yahoo.com.tw>
2015-10-12 14:23:59 +0800

    Add two book discount scenario

--- KataPotter/KataPotterPriceCalculator.cs
+++ KataPotter/KataPotterPriceCalculator.cs
@@ -10,10 +10,17 @@ namespace KataPotter
     {
         private static readonly double BOOK_UNIT_PRICE = 8;
 
+        private static readonly double TWO_BOOKS_DISCOUNT_RATE = 0.95;
+
         public double Calculate(Dictionary<string, int> booksToBuy)
         {
             var totalCount = booksToBuy.Sum(i => i.Value);
             var totalPrice = totalCount * BOOK_UNIT_PRICE;
+
+            if (totalCount > 1)
+            {
+                totalPrice = totalPrice * TWO_BOOKS_DISCOUNT_RATE;
+            }
 
             return totalPrice;
         }
